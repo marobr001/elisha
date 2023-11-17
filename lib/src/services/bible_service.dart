f3ad718dffac69002973534ac7bc4102fb0d46ff
Martin Obrcian <martinobrcian01@gmail.com>
2023-11-17 13:36:06 +0100

    Profiling completed, bottleneck updated

--- lib/src/services/bible_service.dart
+++ lib/src/services/bible_service.dart
@@ -15,13 +15,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
-
 import 'package:dio/dio.dart';
-
 import 'package:elisha/src/config/exceptions.dart';
 import 'package:elisha/src/models/book.dart';
 import 'package:elisha/src/models/chapter.dart';
@@ -31,38 +27,31 @@ import 'package:elisha/src/providers/bible_translations_provider.dart';
 import 'package:elisha/src/repositories/bible_repository.dart';
 
 class BibleService {
-  BibleService();
+  late Future<Map<String, dynamic>> _jsonData;
+  late List<dynamic> _bibleText;
 
-  final jsonText = rootBundle.loadString('backend/$translationAbb.json');
+  BibleService() {
+    _jsonData = _loadJsonData();
+  }
+
+  Future<Map<String, dynamic>> _loadJsonData() async {
+    final text = await rootBundle.loadString('backend/$translationAbb.json');
+    final map = json.decode(text) as Map<String, dynamic>;
+    _bibleText = map['resultset']['row'] as List<dynamic>;
+    return map;
+  }
 
   Future<List<Book>> getBooks(String? bookID) async {
     try {
+      List<dynamic> bookText;
       if (!['', null].contains(bookID)) {
-        final text = await rootBundle.loadString('backend/$translationAbb.json');
-
-        final map = json.decode(text) as Map<String, dynamic>;
-
-        final bibleText = map['resultset']['row'] as List<dynamic>;
-
-        final bookText = bibleText.where((element) => element['field']![1].toString() == bookID).toList();
-
-        final chapters = {
-          for (var item in bookText) ChapterId(id: item['field']![2]),
-        }.toList();
-
-        final book = Book(
-          id: bookText[0]['field']![1],
-          name: BibleRepository().mapOfBibleBooks[int.parse(bookID!)],
-          testament: bookText[0]['field']![1] >= 40 ? 'New' : 'Old',
-          chapters: chapters,
-        );
-
-        return [book];
+        bookText = _bibleText.where((element) => element['field']![1].toString() == bookID).toList();
       } else {
-        final books = BibleRepository().getBooks();
-
-        return books;
+        bookText = _bibleText;
       }
+
+      final books = _extractBooks(bookText);
+      return books;
     } on DioError catch (e) {
       throw Exceptions.fromDioError(e);
     }
@@ -70,30 +59,13 @@ class BibleService {
 
   Future<Chapter> getChapter(String bookID, String chapterID, String translationID) async {
     try {
-      final text = await rootBundle.loadString('backend/$translationAbb.json');
-
       final books = await getBooks(bookID);
 
-      final map = json.decode(text) as Map<String, dynamic>;
+      final chapterText = _bibleText
+          .where((element) => element['field']![1].toString() == bookID && element['field']![2].toString() == chapterID)
+          .toList();
 
-      final bibleText = map['resultset']['row'] as List<dynamic>;
-
-      final bookText = bibleText.where((element) => element['field']![1].toString() == bookID).toList();
-
-      final chapterText = bookText.where((element) => element['field']![2].toString() == chapterID).toList();
-
-      final verses = {
-        for (var item in chapterText) Verse.fromList(item['field']).copyWith(book: books[0]),
-      }.toList();
-
-      final chapter = Chapter(
-        id: int.parse(chapterID),
-        number: chapterID,
-        translation: translationID,
-        verses: verses,
-        bookmarked: false,
-      );
-
+      final chapter = _extractChapter(chapterText, books[0]);
       return chapter;
     } on DioError catch (e) {
       throw Exceptions.fromDioError(e);
@@ -102,29 +74,8 @@ class BibleService {
 
   Future<List<Chapter>> getChapters(String? bookID) async {
     try {
-      final text = await rootBundle.loadString('backend/$translationAbb.json');
-
-      final map = json.decode(text) as Map<String, dynamic>;
-
-      final bibleText = map['resultset']['row'] as List<dynamic>;
-
-      final bookText = bibleText.where((element) => element['field']![1].toString() == bookID).toList();
-
-      final verses = {
-        for (var item in bookText) Verse.fromList(item['field']),
-      }.toList();
-
-      final chapters = [
-        for (var item in bookText)
-          Chapter(
-            id: bookText[0]['field']![2],
-            number: bookText[0]['field']![2].toString(),
-            translation: translationID,
-            verses: verses.where((element) => element.chapterId == item['field']![2]).toList(),
-            bookmarked: false,
-          )
-      ];
-
+      final chaptersText = _bibleText.where((element) => element['field']![1].toString() == bookID).toList();
+      final chapters = _extractChapters(chaptersText);
       return chapters;
     } on DioError catch (e) {
       throw Exceptions.fromDioError(e);
@@ -133,61 +84,84 @@ class BibleService {
 
   Future<List<Verse>> getVerses(String bookID, String chapterID, String? verseID) async {
     try {
-      final text = await rootBundle.loadString('backend/$translationAbb.json');
-
-      final map = json.decode(text) as Map<String, dynamic>;
-
-      final bibleText = map['resultset']['row'] as List<dynamic>;
-
-      final books = await getBooks(bookID);
-
       if (!['', null].contains(verseID)) {
-        var vId = bookID;
-
-        switch (chapterID.length) {
-          case 1:
-            vId += '00' + chapterID;
-            break;
-          case 2:
-            vId += '0' + chapterID;
-            break;
-          default:
-            vId += chapterID;
-        }
-        switch (verseID!.length) {
-          case 1:
-            vId += '00' + verseID;
-            break;
-          case 2:
-            vId += '0' + verseID;
-            break;
-          default:
-            vId += verseID;
-        }
-
-        final verseElement =
-            bibleText.where((element) => element['field']![0].toString() == vId).toList()[0]['field'] as List<dynamic>;
-
-        verseElement[1] = books;
-
-        final verses = [Verse.fromList(verseElement)];
-
+        final vId = _composeVerseId(bookID, chapterID, verseID!);
+        final verseElement = _bibleText.firstWhere((element) => element['field']![0].toString() == vId)['field'] as List<dynamic>;
+        final books = await getBooks(bookID);
+        final verses = [_extractVerse(verseElement, books[0])];
         return verses;
       } else {
-        final bookText = bibleText.where((element) => element['field']![1].toString() == bookID).toList();
-
-        final chapterText = bookText.where((element) => element['field']![2].toString() == chapterID);
-
+        final chapterText = _bibleText.where((element) => element['field']![1].toString() == bookID && element['field']![2].toString() == chapterID);
         final books = await getBooks(bookID);
-
-        final verses = {
-          for (var item in chapterText) Verse.fromList(item['field']).copyWith(book: books[0]),
-        }.toList();
-
+        final verses = _extractVerses(chapterText, books[0]);
         return verses;
       }
     } on DioError catch (e) {
       throw Exceptions.fromDioError(e);
     }
+  }
+
+  List<Book> _extractBooks(List<dynamic> bookText) {
+    final chapters = {
+      for (var item in bookText) ChapterId(id: item['field']![2]),
+    }.toList();
+
+    final book = Book(
+      id: bookText[0]['field']![1],
+      name: BibleRepository().mapOfBibleBooks[int.parse(bookText[0]['field']![1].toString())],
+      testament: bookText[0]['field']![1] >= 40 ? 'New' : 'Old',
+      chapters: chapters,
+    );
+
+    return [book];
+  }
+
+  Chapter _extractChapter(List<dynamic> chapterText, Book book) {
+    final verses = {
+      for (var item in chapterText) Verse.fromList(item['field']).copyWith(book: book),
+    }.toList();
+
+    return Chapter(
+      id: int.parse(chapterText[0]['field']![2].toString()),
+      number: chapterText[0]['field']![2].toString(),
+      translation: translationID,
+      verses: verses,
+      bookmarked: false,
+    );
+  }
+
+  List<Chapter> _extractChapters(List<dynamic> chaptersText) {
+    final verses = {
+      for (var item in chaptersText) Verse.fromList(item['field']),
+    }.toList();
+
+    return [
+      for (var item in chaptersText)
+        Chapter(
+          id: int.parse(item['field']![2].toString()),
+          number: item['field']![2].toString(),
+          translation: translationID,
+          verses: verses.where((element) => element.chapterId == item['field']![2]).toList(),
+          bookmarked: false,
+        )
+    ];
+  }
+
+  Verse _extractVerse(List<dynamic> verseElement, Book book) {
+    return Verse.fromList(verseElement).copyWith(book: book);
+  }
+
+  List<Verse> _extractVerses(Iterable<dynamic> chapterText, Book book) {
+    final verses = {
+      for (var item in chapterText) Verse.fromList(item['field']).copyWith(book: book),
+    }.toList();
+
+    return verses;
+  }
+
+  String _composeVerseId(String bookID, String chapterID, String verseID) {
+    return bookID +
+        chapterID.padLeft(3, '0') +
+        verseID.padLeft(3, '0');
   }
 }
